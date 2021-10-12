191d1296ba5b28078cd01057c401aac1b897b5e0
tovebr <tove.brandt@student.kyh.se>
2021-10-12 12:03:16 +0200

    minor changes with output

--- script.js
+++ script.js
@@ -66,10 +66,10 @@ class Song {
   }
 
   
- let playList = [];
+let playList = [];
 
   //MELI: Function to activate the script at the click of the button
-  addPlaylist.addEventListener("click", function newSong() {
+addPlaylist.addEventListener("click", function newSong() {
     //Collecting values from the input fields
     let artist = document.getElementById("enterArtist").value;
     let song = document.getElementById("enterSong").value;
@@ -91,16 +91,16 @@ class Song {
       
     }
 
-    // playList.push(new Song('Britta', 'One more time', 'pop'));
-    // playList.push(new Song('Hazelnut hobo', 'Covfef', 'rock'));
-    // playList.push(new Song('Dagge', 'Greedy thief', 'country'));
-    // playList.push(new Song('Locomotive Lars', 'chucka cho', 'alternative'));
-    // playList.push(new Song('Helms deep', 'into the mountain', 'indie'));
-    // playList.push(new Song('Abracadabra', 'pick a card', 'pop'));
-    // playList.push(new Song('Meli Montana', 'gumminos', 'hiphop'));
-    // playList.push(new Song('Lens-Amir', 'photograph', 'indie'));
-    // playList.push(new Song('Sebbe af Hans', 'Hansen of sweden', 'classical'));
-    // playList.push(new Song('Tendonitis Tove', 'Strech', 'country'));
+    playList.push(new Song('Britta', 'One more time', 'pop'));
+    playList.push(new Song('Hazelnut hobo', 'Covfef', 'rock'));
+    playList.push(new Song('Dagge', 'Greedy thief', 'country'));
+    playList.push(new Song('Locomotive Lars', 'chucka cho', 'alternative'));
+    playList.push(new Song('Helms deep', 'into the mountain', 'indie'));
+    playList.push(new Song('Abracadabra', 'pick a card', 'pop'));
+    playList.push(new Song('Meli Montana', 'gumminos', 'hiphop'));
+    playList.push(new Song('Lens-Amir', 'photograph', 'indie'));
+    playList.push(new Song('Sebbe af Hans', 'Hansen of sweden', 'classical'));
+    playList.push(new Song('Tendonitis Tove', 'Strech', 'country'));
 
     printSongs();
     cleanInput();
@@ -111,26 +111,19 @@ class Song {
   
   function printSongs() {
 
-    if (playlistDisplay.classList.contains('hidden')) { playlistDisplay.classList.remove('hidden')};
+    if (playlistDisplay.classList.contains('hidden')) playlistDisplay.classList.remove('hidden');
    
-      playList.forEach((item, i) => {
-      let obj = item.songInfo();
-      let playlistItem = document.createElement('div');
-      playlistItem.innerHTML = `
-      <p class="artist-display">${obj.artist}</p>
-      <p class="song-display">${obj.song}</p>
-      <p class="genre-display">${obj.genre}</p>
-      `;
-      playlistDisplay.insertBefore(playlistItem, playlistItem.nextSibling);
+    playList.forEach((item, i) => {
+    let obj = item.songInfo();
+    let playlistItem = document.createElement('div');
 
-      
-      playList.forEach((item, i) => {
-      songOutput += `<p id="song">${item.songInfo()} <i class="fas fa-check-circle"></i><p id="">`;
+    playlistItem.innerHTML = `
+    <p class="artist-display">${obj.artist}</p><p class="song-display">${obj.song}</p><p class="genre-display">${obj.genre}</p><i class="fas fa-check-circle"></i>`;
 
+    playlistDisplay.insertBefore(playlistItem, playlistItem.nextSibling);
     });
-
-  }
   
+   }
   //MELI: Cleans text input with click of a button
   function cleanInput() {
     document.getElementById("enterArtist").value = "";
@@ -142,6 +135,7 @@ class Song {
   cleanOutput.addEventListener("click", function cleanOutput() {
     document.getElementById("output").innerHTML = "";
     document.getElementById("output1").innerHTML = "";
+    if (!playlistDisplay.classList.contains('hidden')) playlistDisplay.classList.add('hidden');
     playList = [];
   });
 
